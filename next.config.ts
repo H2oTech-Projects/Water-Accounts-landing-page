b7ff7b48e916462f5ba93f355ec23740291fae1b
jumirgosain <jumir@h2otechonline.com>
2025-12-26 10:35:45 +0545

    build: updated next js version and next.config.ts file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["wateraccounts.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "wateraccounts.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
